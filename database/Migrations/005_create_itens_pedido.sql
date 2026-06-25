b5a0d4d1385a90012898f22d91f556373b57615c
Weiber210 <joaovitorfloresweiber9@gmail.com>
2026-06-25 08:21:21 -0300

    adicionado tabela Itens_Pedido

---
+++ database/Migrations/005_create_itens_pedido.sql
@@ -0,0 +1,10 @@
+-- Itens Pedidos do Sistema
+ create table itens_pedido(
+  id int auto_increment primary key,
+  pedido_id int not null,
+  produto_id int not null,
+  quantidade int not null,
+  valor_unitario decimal(10,2) not null,
+  foreign key (pedido_id) references pedidos(id),
+  foreign key (produto_id) references produtos(id)
+ );
