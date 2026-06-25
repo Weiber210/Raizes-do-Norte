0fd9c34d7e004de43765f43ef49c67c309fd78e0
Weiber210 <joaovitorfloresweiber9@gmail.com>
2026-06-25 09:27:55 -0300

    Arquitetura de arquivos Seed

--- database/Migrations/003_create_produtos.sql
+++ database/Migrations/003_create_produtos.sql
@@ -1,8 +1,8 @@
 -- Produtos do sistema
 create table produtos(
-  id int auto_increment primary key
-  nome varchar(100) not null
-  descricao varchar(255) not null
-  preco decimal(10,2) not null
+  id int auto_increment primary key,
+  nome varchar(100) not null,
+  descricao varchar(255) not null,
+  preco decimal(10,2) not null,
   ativo boolean not null
  );
