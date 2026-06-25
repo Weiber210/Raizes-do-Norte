88af59532730b1b01c5ece3b0074cdafcb4e8a58
Weiber210 <joaovitorfloresweiber9@gmail.com>
2026-06-25 10:03:15 -0300

    Adicionado Produtos (Seed)

---
+++ database/Seeds/003_seed_produtos.sql
@@ -0,0 +1,5 @@
+-- Produtos do Sistema
+insert into produtos (nome, descricao, preco, ativo) values
+('Bolo de Milho', 'Bolo caseiro tradicional nordestino', 15.99, true),
+('Tapioca Recheada', 'Tapioca com queijo e manteiga', 8.99, true),
+('Cuscuz Nordestino', 'Cuscuz com manteiga e ovos', 11.99, true);
