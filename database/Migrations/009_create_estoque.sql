2a2f2db8b1aebff21ef67d6f3b4d123b841255d2
Weiber210 <joaovitorfloresweiber9@gmail.com>
2026-06-25 08:34:56 -0300

    adicionado tabela estoque

---
+++ database/Migrations/009_create_estoque.sql
@@ -0,0 +1,10 @@
+-- Estoque do Sistema
+ create table estoque(
+  id int auto_increment primary key,
+  produto_id int not null,
+  unidade_id int not null,
+  quantidade int not null,
+  ultima_atualizacao timestamp default current_timestamp,
+  foreign key (produto_id) references produtos(id),
+  foreign key (unidade_id) references unidades(id)
+ );
