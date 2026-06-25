6d768cba10e50907b9d8fa8bbba9ff1a7f192b22
Weiber210 <joaovitorfloresweiber9@gmail.com>
2026-06-25 07:54:39 -0300

    Adicionando Tabela Unidades

--- database/Migrations/001_create_usuarios.sql
+++ database/Migrations/001_create_usuarios.sql
@@ -1,9 +1,10 @@
  -- Usuários do Sistema
+ create table usuarios(
  id int auto_increment primary key
  nome varchar(100) not null
  email varchar(150) not null unique
- senha varchar(255) no null
- perfil varchar(50) no null
+ senha varchar(255) not null
+ perfil varchar(50) not null
  consentimento_lgpd boolean not null
  created_at timestamp default current_timestamp
- 
+ );
---
+++ database/Migrations/002_Create_unidade.sql
@@ -0,0 +1,8 @@
+-- Unidades do sistema
+ create table unidades(
+  id int auto_increment primary key
+  nome varchar(100) not null
+  cidade varchar(255) not null
+  estado varchar(2) not null
+  ativa boolean not null
+ );
