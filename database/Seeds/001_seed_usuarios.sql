c872ca35060adee7cf04ad255b0905d060172946
Weiber210 <joaovitorfloresweiber9@gmail.com>
2026-06-25 09:45:56 -0300

    Adicionado usuário administrador (seed)

---
+++ database/Seeds/001_seed_usuarios.sql
@@ -0,0 +1,8 @@
+-- Administrador do sistema
+insert into usuarios (nome, email, senha, perfil, consentimento_lgpd) values
+(
+'Administrador',
+'admin@system.com',
+'$2y$10$Qm9uZk1vQ0hhbkdlcmFkby5FWEFNUExFS0VZMTIzNDU2Nzg5',
+'ADMIN', true
+);
