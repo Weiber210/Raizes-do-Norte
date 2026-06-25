eac52db43ec3b2578698d2788f8356aa858c27f6
Weiber210 <joaovitorfloresweiber9@gmail.com>
2026-06-25 11:38:11 -0300

    Conexão com o banco

---
+++ config/database.php
@@ -0,0 +1,25 @@
+// localização e conexão com o banco
+<?php
+$host = "localhost";
+$db = "raizes_do_norte";
+$user = "root";
+$pass = "";   
+
+// Tentativa de conexão
+try{
+$pdo = new PDO(
+    "mysql:host=$host;
+    dbname=$db",charset=utf8,
+    $user,
+    $pass
+);
+
+// Mostrar o erro
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch(Exception $erro){
+
+// Mostrar mensagem
+die("ERRO AO CONECTAR AO BANCO DE DADOS: ".$erro->getMessage());
+}
