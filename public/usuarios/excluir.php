10c0292ec856afbdff33c1b988dc66c8ee6fc954
Weiber210 <joaovitorfloresweiber88@gmail.com>
2026-06-26 22:15:40 -0300

    Adicionado funcionalidade de deletar usuário dentro de Usuários

---
+++ public/usuarios/excluir.php
@@ -0,0 +1,19 @@
+<?php
+//Conectar ao banco de ddados
+require_once '../../config/database.php';  
+
+// Localiza o ID
+$id = $_GET['id'] ?? null;
+if(!$id){
+    die("ID do usuário não informado.");
+}
+// Buscar o usuário
+$sql = "delete from usuarios where id = ?";
+$stmt = $pdo->prepare($sql);
+
+$stmt->BindValue(1, $id, PDO::PARAM_INT);
+$stmt->execute();
+
+header("Location: index.php");
+exit;
+?>
