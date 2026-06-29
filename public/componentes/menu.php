76d4c754157f69f859f79026cb9bbcbe2247c61b
Weiber210 <joaovitorfloresweiber9@gmail.com>
2026-06-29 09:39:38 -0300

    Adicionado novas telas ao Menu

--- public/componentes/menu.php
+++ public/componentes/menu.php
@@ -1,4 +1,5 @@
 <?php
+
 $perfilMenu = $_SESSION["perfil"] ?? "";
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark">
@@ -10,20 +11,28 @@ $perfilMenu = $_SESSION["perfil"] ?? "";
         <div class="collapse navbar-collapse" id="menuPrincipal">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item"><a class="nav-link" href="/Raizes-do-Norte/public/pedidos/index.php">Pedidos</a></li>
+                <li class="nav-item"><a class="nav-link" href="/Raizes-do-Norte/public/estoque/index.php">Estoque</a></li>
+
+                <?php if (in_array($perfilMenu, ["Administrador", "Gerente", "Atendente"], true)) { ?>
+                <li class="nav-item"><a class="nav-link" href="/Raizes-do-Norte/public/pagamentos/index.php">Pagamentos</a></li>
+                <li class="nav-item"><a class="nav-link" href="/Raizes-do-Norte/public/fidelidade/index.php">Fidelidade</a></li>
+                <?php } ?>
 
                 <?php if (in_array($perfilMenu, ["Administrador", "Gerente"], true)) { ?>
                 <li class="nav-item"><a class="nav-link" href="/Raizes-do-Norte/public/produtos/index.php">Produtos</a></li>
                 <li class="nav-item"><a class="nav-link" href="/Raizes-do-Norte/public/unidades/index.php">Unidades</a></li>
+                <li class="nav-item"><a class="nav-link" href="/Raizes-do-Norte/public/auditoria/index.php">Auditoria</a></li>
                 <?php } ?>
 
                 <?php if ($perfilMenu === "Administrador") { ?>
                 <li class="nav-item"><a class="nav-link" href="/Raizes-do-Norte/public/usuarios/index.php">Usuários</a></li>
-                <?php } ?>
-
                 <li class="nav-item"><a class="nav-link" href="/Raizes-do-Norte/public/swagger/index.php">Swagger</a></li>
+                <?php } ?>
             </ul>
 
-            <span class="navbar-text me-3"><?= htmlspecialchars($_SESSION["usuario"], ENT_QUOTES, "UTF-8") ?></span>
+            <span class="navbar-text me-3">
+                <?= htmlspecialchars($_SESSION["usuario"], ENT_QUOTES, "UTF-8") ?>
+            </span>
 
             <a class="btn btn-outline-light" href="/Raizes-do-Norte/public/logout.php">Sair</a>
         </div>
